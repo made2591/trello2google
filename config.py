f4081b8d063af39f37cdc602e97abd59108be9dc
valentinarho <rho.valentina@gmail.com>
2014-07-15 11:53:05 +0200

    Commented configuration file

--- config.py
+++ config.py
@@ -5,23 +5,31 @@ __author__ = 'valentinarho'
 __author__ = 'matteomadeddu'
 
 ##### GOOGLE CALENDAR CONFIGURATION
+
+# Setup your Google email and password
 EMAIL = 'trello2google@gmail.com'
 PASS = 'password'
-# You must define the name for the calendar dedicated to Trello events and trello2google app.
+
+# Define the name for the calendar dedicated to Trello events and trello2google app.
 # if the calendar not exists the app will create it for you!
-NOME_CALENDARIO = 'Calendario di trello2google'
+NOME_CALENDARIO = 'Trello Events'
 
 ##### TRELLO CONFIGURATION
-# Get key and secret from
-# PASS
-KEY = "ce165ecac5876ad67fb32azzzzzzzzzz"
-SECRET = "62a8760c7c08aab64cab97a8370265caba6dfc2970f2635016803ac9a8947zzz"
-# Get forever token from
-# https://trello.com/1/authorize?key=__KEY__&name=__NAME_OF_APPLICATION__&expiration=never&response_type=token
-FOREVER_TOKEN = "7570779e2ce8aaf7ede6ce79db0df9dfdbd19059f91a45c42dc5dfa31f456zzz"
 
-# Definite a mano con il token sull'url della board (visibile tramite browser)
+# Application key and secret:
+# Get it from this url: https://trello.com/1/appKey/generate
+KEY = "TODO5ecac5876ad67fb32azzzzzzzzzz"
+SECRET = "TODOs60c7c08aab64cab97a8370265caba6dfc2970f2635016803ac9azzzzzzz"
+
+# Get forever token from visiting this url (first complete the url with your API-KEY)
+# https://trello.com/1/authorize?key=__YOUR-APP-KEY__&name=Trello2Google&expiration=never&response_type=token
+FOREVER_TOKEN = "TODO"
+
+# Boards to convert
+# On the left the board name, on the right the board id!
+# Get your board id from Trello URL: https://trello.com/b/____BOARD-ID____/title-of-board
+# Visit your board from a browser, and copy the ___BOARD-ID___ string.
 BOARDS = {
-    'Cose da vedere' : '51aefecdd2e8a35469000zzz',
-    'Cose da fare' :   '51aeffa426a6793f36001zzz',
+    'Board One' : 'TODOfecdd2e8a35469000zzz',
+    'Board Two' : 'TODOffa426a6793f36001zzz',
 }
